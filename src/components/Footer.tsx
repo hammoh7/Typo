f1e9cdc75318938a61de912b4e95e68805072ba6
hammoh7 <soahammohaadkar@gmail.com>
2024-07-22 20:26:52 +0530

    Components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      © 2024 Typing Speed Test. All rights reserved.
+    </footer>
+  );
+  
+  export default Footer;
+  
