ff37c09654d4e794d0c226d4badfd719da11003c
hammoh7 <soahammohaadkar@gmail.com>
2024-07-26 23:54:51 +0530

    App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import HomeContent from "@/components/Hero";
+import Layout from "@/components/Layout";
+
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col">
-      <Header />
-      <main className="flex-grow flex items-center justify-center px-4 py-16">
-        <HomeContent />
-      </main>
-      <Footer />
-    </div>
+    <Layout>
+      <HomeContent />
+    </Layout>
   );
 };
 
